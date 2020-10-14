a76ffb5fbbc29effe1990564e080217fc296c13f
Beksultan <beksultankaiypov@gmail.com>
2020-10-14 15:36:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,8 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0 
     while element_index < src[row_index].count do 
-      strings += src[row_index][element_index].join
+      src[row_index][element_index]
+      strings += src[row_index][element_index]
       element_index += 1 
     end
     row_index += 1 
