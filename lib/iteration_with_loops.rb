677a5b4e8f66ce642a0364067d67a158a07bfc52
Beksultan <beksultankaiypov@gmail.com>
2020-10-14 15:54:58 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -22,20 +22,3 @@ end
 
 
 
-
-# def join_nested_strings(mixed_data)
-#   string =" "
-#   row_index = 0
-#   while row_index < mixed_data.count do
-#     element_index = 0
-#     while element_index < mixed_data[row_index].count do
-#       if mixed_data[row_index][element_index].class==" ".class
-#       string = string +" " + mixed_data[row_index][element_index]
-#     end
-#       element_index += 1
-#     end
-#     row_index += 1
-#   end
-#   string
-
-# end
